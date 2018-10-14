78d4c24dc7f951085549eb91bc00182cd4dfbfac
Lindsay Braviak <lbraviak@gmail.com>
2018-10-14 22:59:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  return puts "Hello, my name is #{name}!"
+end
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(array)
+  array.each do |name|
+    puts "Hello, my name is #{name}!"
+  end
+end
